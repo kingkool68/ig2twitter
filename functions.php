747fd42f7b9f90e6f89cd9fb967f84d01064b6ac
Russell Heimlich <kingkool68@gmail.com>
2016-06-07 00:17:03 -0400

    Broke out the tweet_media() function to tweet a picture or video depending on the media type of the Instagram post.

--- functions.php
+++ functions.php
@@ -60,21 +60,21 @@ function tweet_media( $media ) {
     $client = new Client();
     $scraper = new Instagram_Scraper();
 
-    // TODO: Differentiate between pictures and video...
-    // SEE: http://stackoverflow.com/questions/32231642/uploading-videos-to-twitter-using-api
-	$id = $media->id;
 	$caption = $media->caption;
     $instagram_url = $scraper->get_permalink( $media->code );
-	$src = $media->display_src;
-	$img = $client->get( $src );
-	$img_body = $img->getBody();
-	file_put_contents ( $id . '.jpg', $img_body );
+	$is_video = $media->is_video;
+	if ( $is_video ) {
+		$twitter_media = tweet_video( $media );
 
-	// Create a Twitter connection object
-	$connection = new TwitterOAuth( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET );
+		// Check and see if the video uploaded successfully. Otherwise tweet out an image instead
+		if ( ! isset( $twitter_media->video ) ) {
+			$is_video = false;
+		}
+	}
 
-	// Get the path of the image to post
-	$twitter_img = getcwd() . '/' . $id . '.jpg';
+	if ( ! $is_video ) {
+		$twitter_media = tweet_picture( $media );
+	}
 
 	// Figure out the maximum length our tweet text can be using URL length variables from https://api.twitter.com/1.1/help/configuration
 	$short_url_length = 23;
@@ -84,23 +84,20 @@ function tweet_media( $media ) {
 	$status = truncate( $caption, $text_limit );
 	$status .= ' ' . $instagram_url;
 
-	// Upload the image to get a media_id to associate with the tweet
-	$media = $connection->upload( 'media/upload', array( 'media' => $twitter_img ) );
 	$args = array(
     	'status' => $status,
 	);
-	if ( isset( $media->media_id_string ) ) {
-		$args['media_ids'] = $media->media_id_string;
+	if ( isset( $twitter_media->media_id_string ) ) {
+		$args['media_ids'] = $twitter_media->media_id_string;
 	}
 
+	$connection = new TwitterOAuth( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET );
 	// Post the tweet to Twitter
 	$result = $connection->post( 'statuses/update', $args );
 	$tweet_id = $result->id_str;
 	$tweet_user = $result->user->screen_name;
 	$tweet_url = 'https://twitter.com/' . $tweet_user . '/status/' . $tweet_id . '/';
 
-    // Cleanup
-	unlink( $id . '.jpg' );
     $output = array(
         'success' => true,
         'tweet_id' => $tweet_id,
@@ -109,4 +106,55 @@ function tweet_media( $media ) {
     );
 
     return $output;
+}
+
+function tweet_picture( $media ) {
+	$client = new Client();
+	$scraper = new Instagram_Scraper();
+
+	$id = $media->id;
+	$src = $media->display_src;
+	$img = $client->get( $src );
+	$img_body = $img->getBody();
+	file_put_contents ( $id . '.jpg', $img_body );
+
+	// Get the path of the image to post
+	$twitter_img = getcwd() . '/' . $id . '.jpg';
+
+	// Create a Twitter connection object
+	$connection = new TwitterOAuth( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET );
+	// Upload the image to get a media_id to associate with the tweet
+	$result = $connection->upload( 'media/upload', array( 'media' => $twitter_img ) );
+	// Cleanup
+	unlink( $id . '.jpg' );
+
+	return $result;
+}
+
+function tweet_video( $media ) {
+	$client = new Client();
+	$scraper = new Instagram_Scraper();
+	$id = $media->id;
+	$single_media = $scraper->get_media( $media->code );
+	$src = $single_media->video_url;
+	$video = $client->get( $src );
+	$video_body = $video->getBody();
+	file_put_contents ( $id . '.mp4', $video_body ); // TODO: grab the actual file extention from the URL
+
+	// Get the path of the video to post
+	$twitter_video = getcwd() . '/' . $id . '.mp4';
+	$twitter_video_filesize = (int) filesize( $twitter_video );
+
+	// Create a Twitter connection object
+	$connection = new TwitterOAuth( TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET );
+	$args = array(
+		'media' => $twitter_video,
+		'media_type' => 'video/mp4',
+	);
+	$result = $connection->upload( 'media/upload', $args, true );
+
+	// Cleanup
+	unlink( $id . '.mp4' );
+
+	return $result;
 }
